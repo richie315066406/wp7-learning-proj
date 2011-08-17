f4010f796b0b27f421cb009c331b94598aa79afb
unknown <kacui@.(none)>
2011-08-17 18:26:30 +0300

    first commit

---
+++ wproj/Ex1CreatingWP7AppWithVS/Begin/HelloPhone/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace HelloPhone
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ClickMeButton_Click(object sender, RoutedEventArgs e)
+        {
+            BannerTextBlock.Text = MessageTextBox.Text;
+            MessageTextBox.Text = String.Empty;
+			this.AnimateBanner.Begin();
+        }
+    }
+}
